85c91b41b9cf7983af618d6451c27776a1eb9671
不爱喝橙子汁 <201410730128@stu.shmtu.edu.cn>
2022-01-13 22:22:37 +0800

    添加相关命令

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const program = require('commander')
 const inquirer = require('inquirer')
 
-const { InitProject, setCompressFile } = require('./cmd')
+const { InitProject, setCompressFile, buildCompressFile } = require('./cmd')
 
 // 初始化命令行工具
 const v1 = program.version('1.0.0', '-v, --version')
@@ -16,6 +16,11 @@ v1.command('init <name>').action(async name => {
 v1.command('reset').action(async () => {
   await inquirer.prompt([])
   await setCompressFile()
+})
+
+v1.command('build <name>').action(async name => {
+  await inquirer.prompt([])
+  await buildCompressFile(name);
 })
 
 program.parse(process.argv)
